6409d80bb3e86034f9c4bbc9a3cc74ef0351aec8
dedyoktavianus65 <86880081+dedyoktavianus65@users.noreply.github.com>
2022-05-30 02:20:34 +0800

    Update routes.php

--- pages/routes.php
+++ pages/routes.php
@@ -1,20 +1,53 @@
-<?php
-if (isset($_GET['page'])) {
+<?php 
+
+if(isset($_GET['page'])) {
     $page = $_GET['page'];
-    switch($page){
-        case '':
-        case 'dashboard':
+    
+    switch($page) {
+        case '';
             file_exists("dashboard.php") ? include "dashboard.php" : include "404.php";
             break;
-        case 'dosen':
-            file_exists("dosen.php") ? include "dosen.php" : include "404.php";
+        case 'dosen';
+            file_exists("dosen/index.php") ?  include "dosen/index.php" : include "404.php";
             break;
-        case 'matakuliah':
-            file_exists("matakuliah.php") ? include "matakuliah.php" : include "404.php";
+        case 'dosen_create';
+            file_exists("dosen/dosen_create.php") ?  include "dosen/dosen_create.php" : include "404.php";
             break;
-        case 'tugas':
-            file_exists("tugas.php") ? include "tugas.php" : include "404.php";
+        case 'dosen_update';
+            file_exists("dosen/dosen_update.php") ?  include "dosen/dosen_update.php" : include "404.php";
             break;
+        case 'dosen_delete';
+            file_exists("dosen/dosen_delete.php") ?  include "dosen/dosen_delete.php" : include "404.php";
+            break;
+
+
+        case 'matakuliah';
+            file_exists("matakuliah/index.php") ?  include "matakuliah/index.php" : include "404.php";
+            break;
+        case 'matakuliah_create';
+            file_exists("matakuliah/matakuliah_create.php") ?  include "matakuliah/matakuliah_create.php" : include "404.php";
+            break;
+        case 'matakuliah_update';
+            file_exists("matakuliah/matakuliah_update.php") ?  include "matakuliah/matakuliah_update.php" : include "404.php";
+            break;
+        case 'matakuliah_delete';
+            file_exists("matakuliah/matakuliah_delete.php") ?  include "matakuliah/matakuliah_delete.php" : include "404.php";
+            break;
+
+
+        case 'tugas';
+            file_exists("tugas/index.php") ?  include "tugas/index.php" : include "404.php";
+            break;
+        case 'tugas_create';
+            file_exists("tugas/tugas_create.php") ?  include "tugas/tugas_create.php" : include "404.php";
+            break;
+        case 'tugas_update';
+            file_exists("tugas/tugas_update.php") ?  include "tugas/tugas_update.php" : include "404.php";
+            break;
+        case 'tugas_delete';
+            file_exists("tugas/tugas_delete.php") ?  include "tugas/tugas_delete.php" : include "404.php";
+            break;
+
         default:
             include "404.php";
             break;
@@ -22,3 +55,4 @@ if (isset($_GET['page'])) {
 } else {
     include "dashboard.php";
 }
+?>
